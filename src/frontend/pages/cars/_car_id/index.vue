2ad4a074e75f5d63bbe4880f1f30a197947041d8
Bailey Lind-Trefts <gibson.bailey@live.com>
2020-05-30 13:30:27 -0700

    Create preliminary car detail page.

---
+++ src/frontend/pages/cars/_car_id/index.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <v-card
+            fluid
+        >
+            Car
+            {{car.name}}
+            {{car.customer_name}}
+        </v-card>
+        <v-card
+            fluid
+        >
+            Comments
+
+        </v-card>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                car: {},
+            }
+        },
+        methods: {
+            get_car() {
+                try {
+                    resp = this.$axios.get(`/api/cars/${this.$route.params.car_id}`)
+                    this.car = resp.data
+                    console.log(this.car)
+                } catch {
+                    console.log("Failed to get car.")
+                }
+            }
+        },
+        async mounted() {
+            this.get_car()
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
